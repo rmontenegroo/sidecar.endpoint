00960da4a6fbe33c558debdf1f9b7d1c7d75b0e5
rodmonte <rodrigo.montenegro@serpro.gov.br>
2020-12-06 19:23:10 -0300

    First release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.1'
+version = '0.1.0'
 
 long_description = (
     'Change history\n'
@@ -31,6 +31,7 @@ setup(name='sidecar.endpoint',
       description="",
       long_description=long_description,
       classifiers=[
+        'Programming Language :: Python :: 2.7',
         'Framework :: Plone',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License'
@@ -38,15 +39,19 @@ setup(name='sidecar.endpoint',
       keywords=('kubernetes', 'container', 'endpoint', 'discovery'),
       author='Rodrigo Montenegro de Oliveira',
       author_email='montenegro.r@gmail.com',
-      url='https://github.com/rmontenegroo/sidecar.endpoint',
+      url='https://github.com/rmontenegroo',
+      project_urls={
+	'Source': 'https://github.com/rmontenegroo/sidecar.endpoint'
+      },
       license='Apache v2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['sidecar', ],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
-                        # -*- Extra requirements: -*-
-                        ],
+      install_requires=[
+         'setuptools', 
+         'plone.app.caching'
+      ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
       test_suite='sidecar.endpoint.tests.test_docs.test_suite',
@@ -57,4 +62,5 @@ setup(name='sidecar.endpoint',
       """,
       setup_requires=["PasteScript"],
       paster_plugins=["ZopeSkel"],
+      python_requires='>=2.7, <3',
       )
